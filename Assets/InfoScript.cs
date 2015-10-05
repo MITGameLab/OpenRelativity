79376a467fae28cbc501fdf5b1b9a59d44ee775c
David ALO <tyoc213@tyoc.mx>
2015-10-05 15:01:09 -0500

    Update InfoScript to use the new UI

--- Assets/InfoScript.cs
+++ Assets/InfoScript.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 using System.Collections;
 
 public class InfoScript : MonoBehaviour {
-	
+	GameObject infoPanel;
     //Gamestate reference for quick access
     GameState state;
 	// Use this for initialization
 	void Start () {
-	
+		GameObject canvas = GameObject.Find ("Canvas");
+		if (canvas != null) {
+			GameObject go = Resources.Load<GameObject>("GameObjects/InfoPanel");
+			if(go != null){
+				infoPanel = Instantiate(go);
+				infoPanel.GetComponent<Transform>().SetParent(canvas.GetComponent<Transform>(), false);
+			}
+		}
         state = GetComponent<GameState>();
 	}
 	
 	//just print out a bunch of information onto the screen.
-	void OnGUI(){
-		//What's our speed of light?
-		GUI.Box (new Rect (0,0,200,100), "Current C: " + state.SpeedOfLight);
-		GUI.Label(new Rect(20,50,200,50), "Increase/Decrease with N/M");
-		//What's our velocity?
-		GUI.Box (new Rect (0,100,200,100), "Current Speed: " + state.PlayerVelocity);
-		GUI.Label(new Rect(20,150,200,50), "Move with with WASD");
-		
-		
-		
+	void Update(){
+		string msg = "Current C: " + state.SpeedOfLight
+				+ "\nIncrease/Decrease with N/M"
+				+ "\n\nCurrent Speed: " + state.PlayerVelocity
+				+ "\nMove with with WASD";
+		UnityEngine.UI.Text text = infoPanel.GetComponentInChildren<UnityEngine.UI.Text> ();
+		text.text = msg;
 	}
 	
 }
