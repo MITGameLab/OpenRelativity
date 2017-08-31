21ace8c6526fd18ed13bb09495ecee4767c5d239
Daniel Strano <stranoj@gmail.com>
2017-08-30 21:03:22 -0400

    Partially reverting materials consolidation
    GameObjects with potentially different positions (and velocities, etc.)
    must have different material instances, since shader parameters need to
    be set differently. However, combining materials is still acceptable
    when meshes are combined.

--- Assets/OpenRelativity/Scripts/GameState.cs
+++ Assets/OpenRelativity/Scripts/GameState.cs
@@ -162,35 +162,36 @@ namespace OpenRelativity
             playerRotation = Vector3.zero;
             deltaRotation = Vector3.zero;
         }
-        public void Start()
-        {
-            //Because we do a lot of manual renderer manipulation,
-            // our materials end up needlessly redundant.
-            // Most of this manipulation happens during "Awake()".
-            // We can optimize by combining redudant materials.
-            MeshRenderer[] renderers = FindObjectsOfType<MeshRenderer>();
-            Dictionary<string, Material> materialDictionary = new Dictionary<string, Material>();
-            for (int i = 0; i < renderers.Length; i++)
-            {
-                MeshRenderer testRenderer = renderers[i];
-                for (int j = 0; j < testRenderer.materials.Length; j++)
-                {
-                    string name = testRenderer.materials[j].name.Replace(" (Instance)", "");
-                    if (materialDictionary.ContainsKey(name))
-                    {
-                        //if (!testRenderer.sharedMaterials[j].Equals(materialDictionary[name]))
-                        //{
-                        //    Destroy(testRenderer.materials[j]);
-                        //}
-                        testRenderer.materials[j] = materialDictionary[name];
-                    }
-                    else
-                    {
-                        materialDictionary.Add(name, testRenderer.materials[j]);
-                    }
-                }
-            }
-        }
+        //This can't be used, because materials have to have distinct shader parameters:
+        //public void Start()
+        //{
+        //    //Because we do a lot of manual renderer manipulation,
+        //    // our materials end up needlessly redundant.
+        //    // Most of this manipulation happens during "Awake()".
+        //    // We can optimize by combining redudant materials.
+        //    MeshRenderer[] renderers = FindObjectsOfType<MeshRenderer>();
+        //    Dictionary<string, Material> materialDictionary = new Dictionary<string, Material>();
+        //    for (int i = 0; i < renderers.Length; i++)
+        //    {
+        //        MeshRenderer testRenderer = renderers[i];
+        //        for (int j = 0; j < testRenderer.materials.Length; j++)
+        //        {
+        //            string name = testRenderer.materials[j].name.Replace(" (Instance)", "");
+        //            if (materialDictionary.ContainsKey(name))
+        //            {
+        //                //if (!testRenderer.sharedMaterials[j].Equals(materialDictionary[name]))
+        //                //{
+        //                //    Destroy(testRenderer.materials[j]);
+        //                //}
+        //                testRenderer.materials[j] = materialDictionary[name];
+        //            }
+        //            else
+        //            {
+        //                materialDictionary.Add(name, testRenderer.materials[j]);
+        //            }
+        //        }
+        //    }
+        //}
         public void reset()
         {
             //Reset everything not level-based
