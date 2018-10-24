bcb3c8be9a1f72f2defae94c87996d1f5725e94d
Daniel Strano <stranoj@gmail.com>
2018-10-23 20:39:42 -0400

    Rindler Horizon
    Due to the player's acceleration, a Rindler horizon should appear at x=-a*c^2/|a|^2. This does not necessarily have to be added to the shaders. A prefab with a quad and a simple script suffices.

---
+++ Assets/OpenRelativity/Scripts/RindlerHorizon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenRelativity
+{
+    public class RindlerHorizon : MonoBehaviour
+    {
+        public GameState state;
+
+        private Renderer myRenderer;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRenderer = GetComponent<Renderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 pAccel = state.PlayerAccelerationVector;
+            float pAccelMag = pAccel.magnitude;
+            if (pAccelMag == 0)
+            {
+                myRenderer.enabled = false;
+            } else
+            {
+                myRenderer.enabled = true;
+                transform.forward = -pAccel / pAccelMag;
+                transform.position = transform.forward * (float)state.SpeedOfLightSqrd / pAccelMag - state.playerTransform.position;
+            }
+        }
+    }
+}
