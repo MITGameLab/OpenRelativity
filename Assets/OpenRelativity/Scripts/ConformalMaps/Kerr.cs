8ecb72eeaffd040b7259f288692d61b76a00acbf
Dan Strano <stranoj@gmail.com>
2021-08-07 18:32:02 -0700

    TimeCoordScale(piw) appears to be identically 1

--- Assets/OpenRelativity/Scripts/ConformalMaps/Kerr.cs
+++ Assets/OpenRelativity/Scripts/ConformalMaps/Kerr.cs
@@ -72,37 +72,6 @@ namespace OpenRelativity.ConformalMaps
             return omega;
         }
 
-        virtual public float TimeCoordScale(Vector3 piw)
-        {
-            // If our "SetEffectiveRadius(piw)" is expected to be exact at the equator, but we use it in all cases,
-            // Then we can better our overall approximation by assuming an inclincation-dependent time coordinate scaling.
-
-            if (spinMomentum <= SRelativityUtil.divByZeroCutoff)
-            {
-                return 1.0f;
-            }
-
-            float a = aParam;
-            float aSqr = a * a;
-
-            float rSqr = piw.sqrMagnitude;
-            // Radius:
-            float r = Mathf.Sqrt(rSqr);
-            // Inclination:
-            float cosInc = piw.z / r;
-            float cosIncSqr = cosInc * cosInc;
-            float sinIncSqr = 1 - cosIncSqr;
-
-            float rrrs = r * (r - schwarzschildRadius);
-
-            float effectiveR = (schwarzschildRadius * r * r) / (r * r + a * a * cosInc * cosInc);
-
-            float kerrScale = Mathf.Sqrt(((aSqr + rSqr) * (aSqr + rSqr) + aSqr * sinIncSqr * (aSqr + rrrs)) / ((aSqr + rrrs) * (aSqr * cosIncSqr + rSqr)));
-            float schwarzScale = 1.0f / Mathf.Sqrt(1.0f - effectiveR / r);
-
-            return kerrScale / schwarzScale;
-        }
-
         override public Comovement ComoveOptical(float properTDiff, Vector3 piw, Quaternion riw)
         {
             if ((spinMomentum <= SRelativityUtil.divByZeroCutoff) || (piw.sqrMagnitude <= SRelativityUtil.divByZeroCutoff))
@@ -114,7 +83,6 @@ namespace OpenRelativity.ConformalMaps
             Quaternion rot = Quaternion.FromToRotation(spinAxis, Vector3.up);
             piw = rot * piw;
 
-            float tFrac = TimeCoordScale(piw);
             SetEffectiveRadius(piw);
 
             // If interior, flip the metric signature between time-like and radial coordinates.
@@ -145,7 +113,7 @@ namespace OpenRelativity.ConformalMaps
             // Apply (full) Schwarzschild ComoveOptical() step.
             piw = Quaternion.Inverse(rot) * piw;
             Comovement forwardComovement = base.ComoveOptical(properTDiff, piw, riw);
-            float tResult = tFrac * forwardComovement.piw.w;
+            float tResult = forwardComovement.piw.w;
             piw = forwardComovement.piw;
             piw = rot * piw;
 
