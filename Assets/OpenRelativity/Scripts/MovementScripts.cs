f6990dc7e8067e1fc8e220b28750f3f11f4df8d9
Dan Strano <stranoj@gmail.com>
2021-08-12 21:22:59 -0700

    Don't evaporate horizons on collision (unrealistically short implied acceleration)

--- Assets/OpenRelativity/Scripts/MovementScripts.cs
+++ Assets/OpenRelativity/Scripts/MovementScripts.cs
@@ -481,7 +481,7 @@ namespace OpenRelativity
                 return;
             }
 
-            Vector3 origPlayerVel = state.PlayerVelocityVector;
+            // Vector3 origPlayerVel = state.PlayerVelocityVector;
 
             Collider myColl = GetComponent<Collider>();
             Vector3 extents = myColl.bounds.extents;
@@ -562,8 +562,8 @@ namespace OpenRelativity
                 }
             }
 
-            Vector3 accel = (state.PlayerVelocityVector - origPlayerVel) / state.FixedDeltaTimePlayer;
-            EvaporateMonopole(state.FixedDeltaTimePlayer, accel);
+            // Vector3 accel = (state.PlayerVelocityVector - origPlayerVel) / state.FixedDeltaTimePlayer;
+            // EvaporateMonopole(state.FixedDeltaTimePlayer, accel);
         }
     }
 }
