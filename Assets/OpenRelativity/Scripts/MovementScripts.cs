fcfa4be7f55f19a83c01de68a5ce4976989321bc
Dan Strano <stranoj@gmail.com>
2021-08-12 20:51:25 -0700

    Fix isMonopoleAccel for player

--- Assets/OpenRelativity/Scripts/MovementScripts.cs
+++ Assets/OpenRelativity/Scripts/MovementScripts.cs
@@ -422,13 +422,8 @@ namespace OpenRelativity
 
             if (myRigidbody != null)
             {
+
                 float ambientTemperature = state.gravityBackgroundTemperature;
-                float schwarzRadius = (8.0f * state.gConst / state.SpeedOfLightSqrd) * myAccel.magnitude;
-                if (schwarzRadius > state.planckLength)
-                {
-                    // state.gravityBackgroundTemperature sets the baseline for 0 proper acceleration.
-                    ambientTemperature += (float)(SRelativityUtil.SchwarzRadiusToPlanckScaleTemp(state.planckLength) - SRelativityUtil.SchwarzRadiusToPlanckScaleTemp(schwarzRadius));
-                }
 
                 float myTemperature = 0;
 
@@ -451,7 +446,8 @@ namespace OpenRelativity
                 // (We ignore the "little bit" of acceleration from collisions, but maybe we could add that next.)
 
                 float surfaceArea = meshFilter.sharedMesh.SurfaceArea() / (state.planckLength * state.planckLength);
-                float dm = SRelativityUtil.sigmaPlanck * surfaceArea * gravitonEmissivity * (Mathf.Pow(myTemperature, 4) - Mathf.Pow(ambientTemperature, 4));
+                float ambientPower = myAccel.magnitude * state.planckPower / state.planckAccel;
+                float dm = SRelativityUtil.sigmaPlanck * surfaceArea * gravitonEmissivity * (Mathf.Pow(myTemperature, 4) - Mathf.Pow(ambientTemperature, 4)) - ambientPower;
 
                 frameDragMass += dm;
                 myRigidbody.mass -= dm;
