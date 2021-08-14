6e3819ef7b86d74c74bbcb51944a924cc242575e
Dan Strano <stranoj@gmail.com>
2021-08-13 18:02:31 -0700

    Interpretive statement

--- Assets/OpenRelativity/Scripts/ConformalMaps/ConformalMap.cs
+++ Assets/OpenRelativity/Scripts/ConformalMaps/ConformalMap.cs
@@ -21,5 +21,7 @@ namespace OpenRelativity.ConformalMaps
 
         // Given an input Unity world coordinate 3-position, tell me the velocity of free fall, i.e. at 0 (gravitational + proper) acceleration.
         abstract public Vector3 GetFreeFallVelocity(Vector3 piw);
+
+        // (A "conformal map" LOOKS like flat space for "matter fields," but "conforms" to an underlying curved space-time manifold.)
     }
 }
