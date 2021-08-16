40f6e75eb6c18eebdca2ee5e02b6087ae5372af7
Dan Strano <stranoj@gmail.com>
2021-08-16 05:39:31 -0700

    Revert "isNonrelativisticShader full PhysX"
    This reverts commit 70db963f05e78101efd24fa300edc00a6c12dddc.

--- Assets/OpenRelativity/Scripts/Objects/RelativisticObject.cs
+++ Assets/OpenRelativity/Scripts/Objects/RelativisticObject.cs
@@ -1203,32 +1203,15 @@ namespace OpenRelativity.Objects
             if (isPhysicsUpdateFrame)
             {
                 // Get the position and rotation after the physics update:
-                if (isNonrelativisticShader)
+                if (!isNonrelativisticShader)
                 {
-                    transform.parent = null;
-                    contractor.position = myRigidbody.position;
-                    transform.parent = contractor;
-                    transform.localPosition = Vector3.zero;
-                }
-
-                riw = myRigidbody.rotation;
-                if (isNonrelativisticShader)
-                {
-                    piw = ((Vector4)transform.position).OpticalToWorld(viw, GetComoving4Acceleration());
-                }
-                else
-                {
+                    riw = myRigidbody.rotation;
                     piw = myRigidbody.position;
                 }
 
                 // Now, update the velocity and angular velocity based on the collision result:
                 viw = vff.AddVelocity(myRigidbody.velocity.RapidityToVelocity(updateMetric));
                 aviw = myRigidbody.angularVelocity / updatePlayerViwTimeFactor;
-
-                if (isNonrelativisticShader)
-                {
-                    ContractLength();
-                }
 
                 if (isMonopoleAccel)
                 {
@@ -1330,9 +1313,10 @@ namespace OpenRelativity.Objects
                 riw = cm.riw;
                 piw = cm.piw;
                 
-                if (myRigidbody != null)
+                if ((myRigidbody != null) && !isNonrelativisticShader)
                 {
-                    myRigidbody.MovePosition(isNonrelativisticShader ? opticalPiw : piw);
+                    myRigidbody.MovePosition(piw);
+                    // We'll MovePosition() for isNonrelativisticShader, further below.
                 }
             }
 
@@ -1408,7 +1392,37 @@ namespace OpenRelativity.Objects
 
             Vector3 properPlusMonopoleAccel = nonGravAccel + leviCivitaDevAccel;
 
-            if (properPlusMonopoleAccel.sqrMagnitude > SRelativityUtil.divByZeroCutoff)
+            if (isNonrelativisticShader)
+            {
+                // Update riw
+                float aviwMag = aviw.magnitude;
+                Quaternion diffRot;
+                if (aviwMag <= SRelativityUtil.divByZeroCutoff)
+                {
+                    diffRot = Quaternion.identity;
+                }
+                else
+                {
+                    diffRot = Quaternion.AngleAxis(Mathf.Rad2Deg * deltaTime * aviwMag, aviw / aviwMag);
+                }
+                riw = riw * diffRot;
+                myRigidbody.MoveRotation(riw);
+
+                // Update piw from "peculiar velocity" in free fall coordinates.
+                piw += deltaTime * peculiarVelocity;
+
+                // Update velocity after position so as not to double-count comovement.
+                // Use viw setter:
+                viw = vff.AddVelocity(peculiarVelocity + deltaTime * properPlusMonopoleAccel);
+
+                transform.parent = null;
+                myRigidbody.MovePosition(opticalPiw);
+                contractor.position = myRigidbody.position;
+                transform.parent = contractor;
+                transform.localPosition = Vector3.zero;
+                ContractLength();
+            }
+            else if (properPlusMonopoleAccel.sqrMagnitude > SRelativityUtil.divByZeroCutoff)
             {
                 // Use viw setter:
                 viw = vff.AddVelocity(peculiarVelocity + deltaTime * properPlusMonopoleAccel);
