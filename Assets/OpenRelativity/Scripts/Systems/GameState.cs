404b3c883cc694674bdbee33d4956f3a0b2f3f71
Daniel Strano <stranoj@gmail.com>
2022-09-23 16:42:31 -0400

    Fix inverse square attentuation

--- Assets/OpenRelativity/Scripts/Systems/GameState.cs
+++ Assets/OpenRelativity/Scripts/Systems/GameState.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.SceneManagement;
 
 using OpenRelativity.ConformalMaps;
 
 namespace OpenRelativity
 {
+    [ExecuteInEditMode]
     public class GameState : MonoBehaviour
     {
         #region Static Variables
@@ -206,6 +211,43 @@ namespace OpenRelativity
         #region consts
         public const int splitDistance = 21000;
         #endregion
+
+        public void OnEnable()
+        {
+            // See https://docs.unity3d.com/Manual/ProgressiveLightmapper-CustomFallOff.html
+            Lightmapping.RequestLightsDelegate testDel = (Light[] requests, Unity.Collections.NativeArray<LightDataGI> lightsOutput) =>
+            {
+                DirectionalLight dLight = new DirectionalLight();
+                PointLight point = new PointLight();
+                SpotLight spot = new SpotLight();
+                RectangleLight rect = new RectangleLight();
+                DiscLight disc = new DiscLight();
+                Cookie cookie = new Cookie();
+                LightDataGI ld = new LightDataGI();
+
+                for (int i = 0; i < requests.Length; i++)
+                {
+                    Light l = requests[i];
+                    switch (l.type)
+                    {
+                        case UnityEngine.LightType.Directional: LightmapperUtils.Extract(l, ref dLight); LightmapperUtils.Extract(l, out cookie); ld.Init(ref dLight, ref cookie); break;
+                        case UnityEngine.LightType.Point: LightmapperUtils.Extract(l, ref point); LightmapperUtils.Extract(l, out cookie); ld.Init(ref point, ref cookie); break;
+                        case UnityEngine.LightType.Spot: LightmapperUtils.Extract(l, ref spot); LightmapperUtils.Extract(l, out cookie); ld.Init(ref spot, ref cookie); break;
+                        case UnityEngine.LightType.Area: LightmapperUtils.Extract(l, ref rect); LightmapperUtils.Extract(l, out cookie); ld.Init(ref rect, ref cookie); break;
+                        case UnityEngine.LightType.Disc: LightmapperUtils.Extract(l, ref disc); LightmapperUtils.Extract(l, out cookie); ld.Init(ref disc, ref cookie); break;
+                        default: ld.InitNoBake(l.GetInstanceID()); break;
+                    }
+                    ld.cookieID = l.cookie?.GetInstanceID() ?? 0;
+                    ld.falloff = FalloffType.InverseSquared;
+                    lightsOutput[i] = ld;
+                }
+            };
+            Lightmapping.SetDelegate(testDel);
+        }
+        void OnDisable()
+        {
+            Lightmapping.ResetDelegate();
+        }
 
         public virtual void Awake()
         {
