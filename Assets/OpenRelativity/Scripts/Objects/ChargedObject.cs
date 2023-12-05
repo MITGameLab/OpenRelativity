d1d07f01369ad0147bb387f17c202ee334a1b2a4
Daniel Strano <dan@unitary.fund>
2023-12-05 09:16:48 -0500

    Magnetic force

--- Assets/OpenRelativity/Scripts/Objects/ChargedObject.cs
+++ Assets/OpenRelativity/Scripts/Objects/ChargedObject.cs
@@ -12,7 +12,7 @@ namespace OpenRelativity.Objects {
         // Maximum force that can be applied.
         private float maxForce = 1e32f;
 
-        void AddElectricForce()
+        void AddElectromagneticForces()
         {
             if (electricCharge <= SRelativityUtil.FLT_EPSILON) {
                 return;
@@ -40,11 +40,25 @@ namespace OpenRelativity.Objects {
                 }
 
                 Vector3 displacement = myRO.piw - otherRO.piw;
+                Vector3 rUnit = displacement.normalized;
+
+                // Electrostatic force:
                 float force = (float)(electricCharge * otherCO.electricCharge / (4 * Mathf.PI * state.vacuumPermittivity * displacement.sqrMagnitude));
                 if (float.IsInfinity(force) || float.IsNaN(force) || (Mathf.Abs(force) > maxForce)) {
-                    myRO.AddForce(((Mathf.Sign(electricCharge) == Mathf.Sign(otherCO.electricCharge)) ? maxForce : -maxForce) * displacement.normalized);
+                    myRO.AddForce(((Mathf.Sign(electricCharge) == Mathf.Sign(otherCO.electricCharge)) ? maxForce : -maxForce) * rUnit);
                 } else {
                     myRO.AddForce(force * displacement.normalized);
+                }
+
+                // Magnetic force:
+                float magneticMag = (float)((state.vacuumPermeability *  otherCO.electricCharge) / (4 * Mathf.PI * displacement.sqrMagnitude));
+                Vector3 magneticField = magneticMag * Vector3.Cross(otherRO.viw, rUnit);
+                Vector3 magneticForce = electricCharge * Vector3.Cross(myRO.viw, magneticField);
+                force = magneticForce.magnitude;
+                if (float.IsInfinity(force) || float.IsNaN(force) || (Mathf.Abs(force) > maxForce)) {
+                    myRO.AddForce(maxForce * magneticForce.normalized);
+                } else {
+                    myRO.AddForce(magneticForce);
                 }
             }
         }
@@ -58,7 +72,7 @@ namespace OpenRelativity.Objects {
         // FixedUpdate is called once before physics update
         void FixedUpdate()
         {
-            AddElectricForce();
+            AddElectromagneticForces();
         }
     }
 }
