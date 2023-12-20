3ca4bdc2c707ad22170feeb440ee72dc5cf0428f
Daniel Strano <dan@unitary.fund>
2023-12-20 12:35:41 -0500

    Perfect 4096x4096 tarot texture

--- Assets/Tarot/Scripts/TarotDeckSides.cs
+++ Assets/Tarot/Scripts/TarotDeckSides.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace PastInfinity
 {
+    [ExecuteInEditMode]
     public class TarotDeckSides : MonoBehaviour
     {
 
@@ -16,11 +17,11 @@ namespace PastInfinity
         const int yTiles = 7;
         const float scaleX = 1.0f / xTiles;
 
-        const float scaleY = 1.0f / yTiles;
+        // const float scaleY = 1.0f / yTiles;
 
         // For perfect 4096x4096 texture:
-        // const float scaleY = (13720.0f / 13848.0f) / yTiles;
-        // const float offsetY = 128.0f / 13848.0f;
+        const float scaleY = (13720.0f / 13848.0f) / yTiles;
+        const float offsetY = 128.0f / 13848.0f;
 
         // Use this for initialization, before relativistic object CombineParent() starts.
         void Awake()
@@ -36,7 +37,7 @@ namespace PastInfinity
             int yIndex = (yTiles - 1) - (catalogCardIndex / xTiles);
 
             //Prepare a new mesh for our split mesh
-            change = Instantiate(meshFilter.mesh);
+            change = meshFilter.sharedMesh;
 
             Vector2 scale = new Vector2(scaleX, scaleY);
             float offsetX = (isLongSide ? 1 : 0) * scaleX;
@@ -44,9 +45,9 @@ namespace PastInfinity
             Vector2[] nUv = new Vector2[change.uv.Length];
             for (int uvIndex = 0; uvIndex < nUv.Length; uvIndex++)
             {
-                nUv[uvIndex] = Vector2.Scale(scale, change.uv[uvIndex]) + new Vector2(scaleX * xIndex + offsetX, scaleY * yIndex);
+                // nUv[uvIndex] = Vector2.Scale(scale, change.uv[uvIndex]) + new Vector2(scaleX * xIndex + offsetX, scaleY * yIndex);
                 // For perfect 4096x4096 texture:
-                // nUv[uvIndex] = Vector2.Scale(scale, change.uv[uvIndex]) + new Vector2(scaleX * xIndex + offsetX, scaleY * yIndex + offsetY);
+                nUv[uvIndex] = Vector2.Scale(scale, change.uv[uvIndex]) + new Vector2(scaleX * xIndex + offsetX, scaleY * yIndex + offsetY);
             }
 
             change.uv = nUv;
