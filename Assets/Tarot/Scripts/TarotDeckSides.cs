904db4ac1d1510df692a43493b819ad762fd9beb
Daniel Strano <dan@unitary.fund>
2023-12-19 21:31:52 -0500

    Smith's Deck (Public Domain source) :hourglass_flowing_sand:

---
+++ Assets/Tarot/Scripts/TarotDeckSides.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace PastInfinity
+{
+    public class TarotDeckSides : MonoBehaviour
+    {
+
+        public bool isLongSide = false;
+
+        private int catalogCardIndex = 160;
+
+        //These store the original and split mesh
+        public Mesh change { get; set; } = null;
+
+        const int xTiles = 24;
+        const int yTiles = 7;
+        const float scaleX = 1.0f / xTiles;
+        const float scaleY = (13720.0f / 13848.0f) / yTiles;
+        const float offsetY = 128.0f / 13848.0f;
+
+        // Use this for initialization, before relativistic object CombineParent() starts.
+        void Awake()
+        {
+            //Grab the meshfilter, and if it's not null, keep going
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            if (meshFilter == null)
+            {
+                return;
+            }
+
+            int xIndex = catalogCardIndex % xTiles;
+            int yIndex = (yTiles - 1) - (catalogCardIndex / xTiles);
+
+            //Prepare a new mesh for our split mesh
+            change = Instantiate(meshFilter.mesh);
+
+            Vector2 scale = new Vector2(scaleX, scaleY);
+            float offsetX = (isLongSide ? 1 : 0) * scaleX;
+
+            Vector2[] nUv = new Vector2[change.uv.Length];
+            for (int uvIndex = 0; uvIndex < nUv.Length; uvIndex++)
+            {
+                nUv[uvIndex] = Vector2.Scale(scale, change.uv[uvIndex]) + new Vector2(scaleX * xIndex + offsetX, scaleY * yIndex + offsetY);
+            }
+
+            change.uv = nUv;
+            meshFilter.mesh = change;
+        }
+    }
+}
